d9194a1e3dcabee94f6029ae5d99b30c110bafc8
Rean <rean@dksoft.com.br>
2020-12-02 12:39:37 -0300

    primeirospassos

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <!-- <ptcliente />-->
+    <ptoperador />
+  </div>
+</template>
+
+<script>
+//import ptcliente from './components/ptcliente.vue'
+import ptoperador from './components/ptoperador.vue'
+
+export default {
+  name: 'App',
+  components: {
+    //ptcliente,
+    ptoperador
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+</style>
---
+++ src/components/ptoperador.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="fundototal">
+    <div class="hello">
+      <div>
+        <input class="caixavisualizarmensagem" />
+      </div>
+      <div>
+        <textarea class="caixaescrevermensagem" />
+        <button class="btnenviarmsg">Enviar</button>
+      </div>
+    </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'recebermsg',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.fundototal{  
+  position:absolute;
+  background-color: rgb(238, 238, 238);
+  width: 100%;
+  height: 100%;
+  top: 0px;
+  left: 0px;
+}
+.hello{
+  position:absolute;
+  margin-top: 20px;
+  left: 27%;
+  width:760px;
+  height:700px;
+  border: 1px solid rgb(141, 141, 141);
+  border-radius: 10px;
+}
+.caixavisualizarmensagem{
+  position: absolute;
+  margin-top: 20px;
+  margin-left: 10px;
+  width: 600px;
+  height: 580px;
+  border: 1px solid rgb(141, 141, 141);
+  border-radius: 10px;  
+  font-family: Verdana, Geneva, Tahoma, sans-serif; 
+}
+.caixaescrevermensagem{
+  position: absolute;
+  margin-top: 620px;
+  margin-left: 10px;
+  width: 602px;
+  height: 50px;
+  border: 1px solid rgb(141, 141, 141);
+  border-radius: 10px;
+  font-family: Verdana, Geneva, Tahoma, sans-serif; 
+}
+.btnenviarmsg{
+  position: absolute;
+  margin-top: 640px;
+  margin-left: 630px;
+  width: 120px;
+  height: 35px;
+  background-color: rgb(141, 141, 141);
+  border-radius: 10px;
+  color: #fff;
+}
+
+</style>
